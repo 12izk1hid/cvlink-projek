3e84d45f72ca3e7aa434b5ce436778c59bdbcec8
12izk1hid <hidayahrizki349@gmail.com>
2024-11-21 22:40:08 +0700

    update

--- app/Views/clients/order.php
+++ app/Views/clients/order.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="<?= base_url('assets/css/keranjang.css') ?>">
 
+
+<!-- Form untuk proses order -->
 <form class="form" method="post" action="<?= base_url('client/order/save') ?>">
     <div class="modal-body">
         <!-- Tabel Keranjang -->
@@ -10,36 +12,38 @@
                 </tr>
             </thead>
             <tbody>
+                <!-- Cek apakah ada data di keranjang -->
                 <?php if (!empty($keranjangDetails)): ?>
                     <?php foreach ($keranjangDetails as $index => $item): ?>
                         <tr>
+                            <!-- Nama Layanan -->
                             <td class="toggle-details" data-id="<?= $item['id'] ?>" data-target="#details-<?= $index ?>" style="cursor: pointer;">
                                 <?= htmlspecialchars($item['nama_service'], ENT_QUOTES, 'UTF-8') ?>
-                                <i class="fa fa-chevron-down"></i>
+                                <i class="fa fa-chevron-down"></i> <!-- Ikon panah -->
                             </td>
                         </tr>
+
+                        <!-- Detail Barang Tersembunyi -->
                         <tr id="details-<?= $index ?>" class="details-row" style="display: none;">
+                            <td colspan="1">
                                 <table class="table barang-table">
                                     <thead>
                                         <tr>
                                             <th>Nama Barang</th>
-                                            <th>Merk</th>
                                             <th>Jumlah</th>
-                                            <th>Harga</th>
-                                            <th>Total Harga</th>
+                                            <th>Harga Satuan</th>
+                                            <th>Besaran</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <!-- Loop untuk menampilkan data barang berdasarkan id layanan -->
+                                        <!-- Data Barang -->
                                         <?php if (isset($keranjangBarang[$item['id']])): ?>
                                             <?php foreach ($keranjangBarang[$item['id']] as $barang): ?>
                                                 <tr>
                                                     <td><?= htmlspecialchars($barang['nama'], ENT_QUOTES, 'UTF-8') ?></td>
-                                                    <td><?= htmlspecialchars($barang['merk'] ?? '-', ENT_QUOTES, 'UTF-8') ?></td>
-                                                    <td> <?= htmlspecialchars($barang['jumlah'] ?? '-', ENT_QUOTES, 'UTF-8') ?> </td>
-                                                    <td><?= htmlspecialchars($barang['harga'] ? number_format($barang['harga'], 0, ',', '.') : '-', ENT_QUOTES, 'UTF-8') ?> per 
-                                                        <?= htmlspecialchars($barang['besaran'] ?? '-', ENT_QUOTES, 'UTF-8') ?></td>
-                                                    <td><?= number_format($barang['harga'] * $barang['jumlah'], 0, ',', '.') ?></td>
+                                                    <td><?= htmlspecialchars($barang['jumlah'], ENT_QUOTES, 'UTF-8') ?></td>
+                                                    <td><?= number_format($barang['harga'], 0, ',', '.') ?> per <?= htmlspecialchars($barang['besaran'], ENT_QUOTES, 'UTF-8') ?></td>
+                                                    <td><?= htmlspecialchars($barang['besaran'], ENT_QUOTES, 'UTF-8') ?></td>
                                                 </tr>
                                             <?php endforeach; ?>
                                         <?php else: ?>
@@ -50,7 +54,6 @@
                             </td>
                         </tr>
                     <?php endforeach; ?>
-
                 <?php else: ?>
                     <tr>
                         <td colspan="5">Keranjang kosong.</td>
@@ -60,6 +63,7 @@
         </table>
     </div>
 </form>
+
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
